4190162c27119e2a398d801db5453454ee999a82
Khalid <kar.khalid@gmail.com>
2020-05-03 01:53:46 +0000

    chore: lint project

--- src/Slider.js
+++ src/Slider.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 
-const Slider = ({bpm, onBpmChange}) => {
+const Slider = ({ bpm, onBpmChange }) => {
   return (
     <div>
       <label htmlFor="bpm">{bpm}</label>
-      <input type="range" id="bpm" name="bpm" min="0" max="400" value={bpm} onChange={onBpmChange}/>
+      <input type="range" id="bpm" name="bpm" min="0" max="400" value={bpm} onChange={onBpmChange} />
     </div>
-  )
+  );
 };
 
 export default React.memo(Slider);
