5be50f06e9ac0a5759f6e74b18a3f8e74b1e01e2
Khalid <kar.khalid@gmail.com>
2020-12-07 00:33:36 +0100

    chore(linting): change linting config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,9 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
+    'jest/globals': true,
   },
-  extends: ['plugin:react/recommended', 'airbnb'],
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -15,9 +16,11 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react'],
+  plugins: ['react', 'react-hooks', 'jest'],
   rules: {
     'react/prop-types': 'off',
     'react/jsx-props-no-spreading': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 };
